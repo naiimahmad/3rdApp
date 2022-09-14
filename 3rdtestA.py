644ceb39425fe5f19bb8a167484fa272f951438c
naiimahmad <112921568+naiimahmad@users.noreply.github.com>
2022-09-14 10:02:11 +0800

    Update 3rdtestA.py

--- 3rdtestA.py
+++ 3rdtestA.py
@@ -1,42 +1,14 @@
-import matplotlib.pyplot as plt
-import numpy as np
+# Perform the prediction by using the trained model
+step_size = 0.01
 
-from sklearn.datasets import make_blobs
-X, y_true = make_blobs(n_samples=300, centers=4,
-                       cluster_std=0.60, random_state=0)
-plt.scatter(X[:, 0], X[:, 1], s=50);
+# Plot the Decision Boundaries
+x_min, x_max = min(X.iloc[:,0]) - 1, max(X.iloc[:,0]) + 1
+y_min, y_max = min(X.iloc[:,1]) - 1, max(X.iloc[:,1]) + 1
+x_values, y_values = np.meshgrid(np.arange(x_min,x_max,step_size), np.arange(y_min,y_max,step_size))
 
-from sklearn.cluster import KMeans
-kmeans = KMeans(n_clusters=4)
-kmeans.fit(X)
-y_kmeans = kmeans.predict(X)
+# Predict labels for all points in the mesh
+predictions = kmeans.predict(np.c_[x_values.ravel(), y_values.ravel()]) 
 
-plt.scatter(X[:, 0], X[:, 1], c=y_kmeans, s=50, cmap='viridis')
-
-centers = kmeans.cluster_centers_
-plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5);
-
-from sklearn.metrics import confusion_matrix
-import seaborn as sns; sns.set()  # for plot styling
-mat = confusion_matrix(digits.target, labels)
-sns.heatmap(mat.T, square=True, annot=True, 
-            fmt='d', cbar=False,
-            xticklabels=digits.target_names,
-            yticklabels=digits.target_names)
-plt.xlabel('true label')
-plt.ylabel('predicted label');
-
-plt.scatter(X['Annual_Income_(k$)'], X['Spending_Score']);
-
-
-from sklearn.decomposition import PCA
-
-pca = PCA().fit(X_train)
-plt.plot(np.cumsum(pca.explained_variance_ratio_))
-plt.xlabel('number of components')
-plt.ylabel('cumulative explained variance');
-
-accum_explained_var = np.cumsum(pca.explained_variance_ratio_)
-
-min_threshold = np.argmax(accum_explained_var > 0.90) # use 90%
-
+# Plot the results
+predictions = predictions.reshape(x_values.s
+some code from internet with modification
